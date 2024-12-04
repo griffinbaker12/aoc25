c8cf8eadc52eaade6711c23cff3fbfb74d4df305
griffin baker <griffin_baker12@icloud.com>
2024-12-04 16:17:09 -0600

    fixed part 2

--- day_01/hn.py
+++ day_01/hn.py
@@ -13,3 +13,10 @@ t = list(
     )
 )
 print(sum(abs(a - b) for a, b in zip(*t)))
+
+l, r = list(
+    zip(
+        *[list(map(int, line.split())) for line in open("test.txt").read().splitlines()]
+    )
+)
+print(sum(v * r.count(v) for v in l))
