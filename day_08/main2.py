9d48dc0bba4bde65a265284d5849e87479ceae17
griffin baker <griffin_baker12@icloud.com>
2024-12-08 15:16:03 -0600

    alt p2 for d9

--- day_08/main2.py
+++ day_08/main2.py
@@ -12,7 +12,10 @@ for r, row in enumerate(grid):
 antinodes = set()
 for arr in antennas.values():
     for i in range(len(arr)):
-        for j in range(i + 1, len(arr)):
+        # for j in range(1, len(arr))
+        for j in range(len(arr)):
+            if i == j:
+                continue
             r1, c1 = arr[i]
             r2, c2 = arr[j]
             r_diff, c_diff = r2 - r1, c2 - c1
@@ -20,9 +23,9 @@ for arr in antennas.values():
                 antinodes.add((r1, c1))
                 r1 -= r_diff
                 c1 -= c_diff
-            while 0 <= r2 < len(grid) and 0 <= c2 < len(grid[0]):
-                antinodes.add((r2, c2))
-                r2 += r_diff
-                c2 += c_diff
+            # while 0 <= r2 < len(grid) and 0 <= c2 < len(grid[0]):
+            #     antinodes.add((r2, c2))
+            #     r2 += r_diff
+            #     c2 += c_diff
 
 print(len(antinodes))
