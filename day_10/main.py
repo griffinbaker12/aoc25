9dd86ee6273764ae09549988767d812324e0a800
griffin baker <griffin_baker12@icloud.com>
2024-12-10 10:12:36 -0600

    update

--- day_10/main.py
+++ day_10/main.py
@@ -6,14 +6,15 @@ dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
 # when I set a default set here, the answer was wrong? but when I passed one in below, it worked
-def walk(trails, r, c, nines):
+def walk(trails, r, c, nines=set()):
     for dr, dc in dirs:
-        if 0 <= r + dr < len(trails) and 0 <= c + dc < len(trails[0]):
-            if trails[r + dr][c + dc] == trails[r][c] + 1:
-                if trails[r + dr][c + dc] == 9:
-                    nines.add((r + dr, c + dc))
+        new_r, new_c = r + dr, c + dc
+        if 0 <= r + dr < len(trails) and 0 <= new_c < len(trails[0]):
+            if trails[new_r][new_c] == trails[r][c] + 1:
+                if trails[new_r][new_c] == 9:
+                    nines.add((new_r, new_c))
                 else:
-                    walk(trails, r + dr, c + dc, nines)
+                    walk(trails, new_r, new_c, nines)
     return len(nines)
 
 
@@ -23,7 +24,10 @@ with open("input.txt") as f:
     for r, row in enumerate(trails):
         for c, char in enumerate(row):
             if char == 0:
+                # GOOD: need new set for each trailhead
                 total += walk(trails, r, c, set())
+                # BAD
+                # total += walk(trails, r, c)
 
 
 print(total)
